02061b61bda92901ff42abe1b85ae8564d6ae766
忘羽梦汐 <wymx_eilauk@163.com>
2025-08-13 04:28:25 +0800

    Update launch.lua

--- config/launch.lua
+++ config/launch.lua
@@ -1,43 +1,42 @@
+---------------------------------------------------------------------------------------------------------------
+-- wezterm默认启动shell配置                                                                                  --
+-- 作者:忘羽梦汐                                                                                             --
+-- 功能介绍:这段代码是一个基于当前操作系统平台（Windows、macOS、Linux）动态配置的终端启动程序（default_prog  --
+--          和启动菜单项（launch_menu）的配置表，常用于终端模拟器（比如 WezTerm）中                          --
+---------------------------------------------------------------------------------------------------------------
+
 local platform = require("utils.platform")()
 
+local icons = {
+        bash = "",
+        zsh = "",
+        ohmyzsh = "",
+        fish = "",
+        powershell = "",
+        cmd = "",
+}
+
 local options = {
-	default_prog = {},
-	launch_menu = {},
+        default_prog = {},
+        launch_menu = {},
 }
 
 if platform.is_win then
-	options.default_prog = { "wsl", "zsh" }
-	options.launch_menu = {
-		{ label = " Zsh (WSL)", args = { "wsl", "zsh" } },
-		{ label = " PowerShell v1", args = { "powershell" } },
-		{ label = " PowerShell v7", args = { "pwsh" } },
-		{ label = " Cmd", args = { "cmd" } },
-		{ label = " Nushell", args = { "nu" } },
-		{
-			label = " GitBash",
-			args = { "C:\\soft\\Git\\bin\\bash.exe" },
-		},
-		{
-			label = " AlmaLinux",
-			args = { "ssh", "kali@192.168.44.147", "-p", "22" },
-		},
-	}
-elseif platform.is_mac then
-	options.default_prog = { "zsh", "--login" }
-	options.launch_menu = {
-		{ label = " Bash", args = { "bash", "--login" } },
-		{ label = " Fish", args = { "/opt/homebrew/bin/fish", "--login" } },
-		{ label = " Nushell", args = { "/opt/homebrew/bin/nu", "--login" } },
-		{ label = " Zsh", args = { "zsh", "--login" } },
-	}
-elseif platform.is_linux then
-	options.default_prog = { "zsh", "--login" }
-	options.launch_menu = {
-		{ label = " Bash", args = { "bash", "--login" } },
-		{ label = " Fish", args = { "/opt/homebrew/bin/fish", "--login" } },
-		{ label = " Nushell", args = { "/opt/homebrew/bin/nu", "--login" } },
-		{ label = " Zsh", args = { "zsh", "--login" } },
-	}
+        -- Windows 默认启动 cmd
+        options.default_prog = { "cmd" }
+        options.launch_menu = {
+                { label = icons.cmd .. " Cmd", args = { "cmd" } },
+                { label = icons.powershell .. " PowerShell", args = { "powershell" } },
+        }
+elseif platform.is_mac or platform.is_linux then
+        -- macOS 和 Linux 默认启动 zsh
+        options.default_prog = { "zsh", "--login" }
+        options.launch_menu = {
+                { label = icons.bash .. " Bash", args = { "bash", "--login" } },
+                { label = icons.zsh .. " Zsh", args = { "zsh", "--login" } },
+                { label = icons.ohmyzsh .. " Oh-My-Zsh", args = { "zsh", "--login", "-i", "-c", "source ~/.zshrc" } },
+                { label = icons.fish .. " Fish", args = { "fish", "--login" } },
+        }
 end
 
 return options
