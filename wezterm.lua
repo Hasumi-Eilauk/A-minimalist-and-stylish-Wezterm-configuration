2e4dd7e223a17472b5c799d3daab581cd853a251
忘羽梦汐 <wymx_eilauk@163.com>
2025-08-13 07:43:59 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -4,10 +4,21 @@ require("events.right-status").setup()
 require("events.tab-title").setup()
 require("events.new-tab-button").setup()
 
-return Config:init()
-  :append(require("config.appearance"))
-  :append(require("config.bindings"))
-  :append(require("config.domains"))
-  :append(require("config.fonts"))
-  :append(require("config.general"))
-  :append(require("config.launch")).options
+local base_config = Config:init()
+        :append(require("config.appearance"))
+        :append(require("config.bindings"))
+        :append(require("config.domains"))
+        :append(require("config.fonts"))
+        :append(require("config.general"))
+        :append(require("config.launch")).options
+
+-- 添加鼠标右键粘贴模块
+base_config.mouse_bindings = base_config.mouse_bindings or {}
+
+table.insert(base_config.mouse_bindings, {
+        event = { Up = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = require("wezterm").action.PasteFrom("Clipboard"),
+})
+
+return base_config
