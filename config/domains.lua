e53e4c49d9bc680aa8c6b2ce3009abb242806f12
忘羽梦汐 <wymx_eilauk@163.com>
2025-08-13 04:23:58 +0800

    Update domains.lua

--- config/domains.lua
+++ config/domains.lua
@@ -1,69 +1,43 @@
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- WezTerm SSH配置                                                                                                                                                 --
+-- 作者:忘羽梦汐                                                                                                                                                   --
+-- 功能介绍:这段代码是 WezTerm 终端模拟器的配置片段，主要用于定义不同类型的远程连接域（Domain），包括 SSH 连接、Unix 域套接字连接和 Windows 子系统 Linux (WSL) 连接--
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 return {
-	-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-	ssh_domains = {
-		{
-			-- The connection is an ssh connection, don't use any multiplexing.
-			multiplexing = "None",
+        -- SSH 远程连接域配置
+        ssh_domains = {
+                {
+                        name = "Kali-linux", -- 唯一名称
+                        remote_address = "192.168.44.147:22", -- 远程地址与端口
+                        username = "kali", -- 登录用户名
+                        multiplexing = "None", -- 关闭多路复用，避免兼容性问题
+                        ssh_option = {
+                                identityfile = "C:\\Users\\Fizz\\.ssh\\id_rsa", -- SSH 私钥路径
+                        },
+                },
+                {
+                        name = "Alma-linux",
+                        remote_address = "host.myalmalinux.com:22",
+                        username = "root",
+                        multiplexing = "None",
+                        ssh_option = {
+                                identityfile = "C:\\Users\\Fizz\\.ssh\\id_rsa",
+                        },
+                },
+        },
 
-			-- The name of this specific domain. Must be unique amongst
-			name = "Kali-linux",
+        -- Unix 域套接字配置（暂未使用）
+        unix_domains = {},
 
-			-- identifies the host:port pair of the remote server
-			-- Can be a DNS name or an IP address with an optional
-			-- ":port" on the end.
-			remote_address = "192.168.44.147:22",
-
-			-- Whether agent auth should be disabled.
-			-- Set to true to disable it.
-			-- no_agent_auth = false,
-
-			-- The username to use for authenticating with the remote host
-			username = "kali",
-
-			-- Specify a SSH connection authentication file
-			-- Default authentication file is "~/.ssh/id_rsa"
-			ssh_option = {
-				identityfile = "C:\\Users\\Fizz\\.ssh\\id_rsa",
-			},
-		},
-		{
-			-- The connection is an ssh connection, don't use any multiplexing.
-			multiplexing = "None",
-
-			-- The name of this specific domain. Must be unique amongst
-			name = "Alma-linux",
-
-			-- identifies the host:port pair of the remote server
-			-- Can be a DNS name or an IP address with an optional
-			-- ":port" on the end.
-			remote_address = "host.myalmalinux.com:22",
-
-			-- Whether agent auth should be disabled.
-			-- Set to true to disable it.
-			-- no_agent_auth = false,
-
-			-- The username to use for authenticating with the remote host
-			username = "root",
-
-			-- Specify a SSH connection authentication file
-			-- Default authentication file is "~/.ssh/id_rsa"
-			ssh_option = {
-				identityfile = "C:\\Users\\Fizz\\.ssh\\id_rsa",
-			},
-		},
-	},
-
-	-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
-	unix_domains = {},
-
-	-- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-	wsl_domains = {
-		{
-			name = "WSL:Ubuntu",
-			distribution = "Ubuntu",
-			username = "kevin",
-			default_cwd = "/home/kevin",
-			default_prog = { "fish" },
-		},
-	},
+        -- WSL 连接配置
+        wsl_domains = {
+                {
+                        name = "WSL:Ubuntu",
+                        distribution = "Ubuntu", -- WSL 发行版名
+                        username = "kevin", -- 登录用户名
+                        default_cwd = "/home/kevin", -- 默认工作目录
+                        default_prog = { "fish" }, -- 默认启动 shell
+                },
+        },
 }
